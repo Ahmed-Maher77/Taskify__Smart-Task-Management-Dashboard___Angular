1e0745bae836e1542c76eee113cef4fd4f3f9ab1
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-20 11:32:43 +0200

    Refactor app component to include Header, Footer, and Dashboard components, removing placeholder content from app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
