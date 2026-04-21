848b2eb0228ff22b72441a453030db2c9baf1b6d
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-21 13:52:16 +0200

    Update app component to include RouterOutlet for enhanced routing capabilities

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { Dashboard } from './pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Dashboard],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
