6a001672e645d3fc684537da53c67fd482b2e00d
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-23 21:53:36 +0200

    Add Session Timer component to app layout for enhanced user session tracking

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { SessionTimer } from './components/session-timer/session-timer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, RouterOutlet],
+  imports: [Header, SessionTimer, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
